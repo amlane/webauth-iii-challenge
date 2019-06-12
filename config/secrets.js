6e37b48d93a49c0edf6deadf5c5a461f6b985097
amlane <twelvexstring@gmail.com>
2019-06-12 14:59:20 -0500

    tested end points and token for login and user access

---
+++ config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || "you gotta pay the troll toll to get into the boy's soul"
+};
--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
-// const restricted = require('../auth/restricted-middleware.js');
+const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', (req, res) => { //removed restricted to test
+router.get('/', restricted, (req, res) => { //removed restricted to test
   Users.find()
     .then(users => {
       res.json(users);
